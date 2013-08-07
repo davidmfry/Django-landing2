4ac4bae652114a47f87d52de405240a7e19ab1bd
David Fry <david.fry.tv@gmail.com>
2013-08-07 08:09:53 -0700

    nothing added

--- landing2/urls.py
+++ landing2/urls.py
@@ -5,13 +5,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'landing2.views.home', name='home'),
-    # url(r'^landing2/', include('landing2.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^$', 'join.views.home', name='home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
